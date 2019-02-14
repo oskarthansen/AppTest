86d6ce558210f713258dd9b4433703fea25adc6f
Oskar Thorin Hansen <oskarthansen@Oskars-iMac.local>
2019-02-14 22:30:44 +0100

    Added new Xamarin test

---
+++ AwesomeApp/AwesomeApp/Models/Note.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace AwesomeApp.Models
+{
+    public class Note
+    {
+        public Note()
+        {
+
+        }
+
+        public string Text { get; set; }
+        public string Filename { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
